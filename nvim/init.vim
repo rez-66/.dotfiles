a159086b9fef6776d13ba57e969683b795a11bb9
reza <rezab66@protonmail.com>
2021-10-15 03:07:48 +0330

    lua babyyy

--- nvim/init.vim
+++ nvim/init.vim
@@ -4,64 +4,16 @@ set relativenumber
 
 call plug#begin('~/.vim/plugged')
 
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} 
-Plug 'nvim-treesitter/playground'
 Plug 'neovim/nvim-lspconfig'
-Plug 'NLKNguyen/papercolor-theme'
-Plug 'akinsho/bufferline.nvim'
-Plug 'vv9k/bogster'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/nvim-cmp'
+
+
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
 
 call plug#end()
 
-set guicursor=i:block
 
-syntax on
-colorscheme bogster 
-
-
-"Telescope bindings"
-nnoremap <leader>ff <cmd>Telescope find_files<cr>
-nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-nnoremap <leader>fb <cmd>Telescope buffers<cr>
-nnoremap <leader>fh <cmd>Telescope help_tags<cr>
-
-"neovim terminal bindings"
-nnoremap <leader>t <cmd>vs<cr><cmd>terminal<cr>
-tnoremap <Esc> <C-\><C-n>
-
-"" remaps
-nnoremap <leader>n <cmd>bn<cr>
-nnoremap <leader>p <cmd>bn<cr>
-
-lua << EOF
-
-require "nvim-treesitter.configs".setup {
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = false, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  }
-}
-
-require("bufferline").setup{}
-
-EOF
-
-runtime lsp.vim
+lua require('lsp')
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,8 +1,42 @@
-lua << EOF
-local nvim_lsp = require('lspconfig')
+local cmp = require'cmp'
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- For `vsnip` user.
+			vim.fn["vsnip#anonymous"](args.body)
+
+			-- For `luasnip` user.
+			-- require('luasnip').lsp_expand(args.body)
+
+			-- For `ultisnips` user.
+			-- vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	mapping = {
+		['<C-d>'] = cmp.mapping.scroll_docs(-4),
+		['<C-f>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.close(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+
+		-- For vsnip user.
+		{ name = 'vsnip' },
+
+		-- For luasnip user.
+		-- { name = 'luasnip' },
+
+		-- For ultisnips user.
+		-- { name = 'ultisnips' },
+
+		{ name = 'buffer' },
+	}
+})
+
+
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -34,15 +68,23 @@ local on_attach = function(client, bufnr)
 
 end
 
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { 'pyright', 'rust_analyzer', 'tsserver','gopls' }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    }
-  }
-end
-EOF
+
+-- Setup lspconfig.
+require('lspconfig').gopls.setup {
+	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	cmd = {'gopls', '--remote=auto'},
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+	},
+	on_attach = on_attach,
+}
+
+require('lspconfig').pyright.setup {
+	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	on_attach = on_attach,
+}
